9565229a4e2461c5741c0e69eab3b4262c566d3e
Jessie <77005672+jiexin-liu@users.noreply.github.com>
2022-04-05 13:06:43 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import * as React from "react";
-import { fetchUtils, Admin, Resource} from 'react-admin';
+import { fetchUtils, Admin, Resource, ListGuesser} from 'react-admin';
 import { UserList } from "./users";
 import { PostCreate, PostEdit, PostList } from "./Posts";
 import simpleRestProvider from 'ra-data-simple-rest';
-import PostIcon from '@material-ui/icons/Book';
-import UserIcon from '@material-ui/icons/Group';
 import Dashboard from "./Dashboard";
-import myDataProvider from "./Dataprovider";
 import { projectList } from "./Project";
 
 
-const fetchJson = (url, options = {}) => {
+const httpClient = (url, options = {}) => {
     options.user = {
-        authenticated: true,
-        token: 'WpCINI6OGVXmv0rIYya4RHyqNe4t0eel'
-    };
+      authenticated: true,
+      token: 'WpCINI6OGVXmv0rIYya4RHyqNe4t0eel',
+    }
     return fetchUtils.fetchJson(url, options);
-};
-const dataProvider = simpleRestProvider('https://api.baserow.io/api/database/fields/table/56854/', fetchJson);
+  }
+
+const dataProvider = simpleRestProvider('https://api.baserow.io/api/database/fields/table/56854/');
+ // const dataProvider = dataProvider('https://api.baserow.io/api/database/fields/table/56854/', httpClient);
+ // console.log(dataProvider);
+
 const App = () => (
         <Admin dashboard={Dashboard} dataProvider={dataProvider}>
-            <Resource name= "Project" list={projectList}/>
+            <Resource name= "projectList" list={projectList}/>
         </Admin>
     );
     
